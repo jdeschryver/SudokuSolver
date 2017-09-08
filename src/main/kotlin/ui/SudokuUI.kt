03a2f1b2ee0ce693a7f6b1f690a08f3aa62ba85c
kkriske <kristoffel.columbus@hotmail.com>
2017-09-09 01:20:36 +0200

    MOAARRRRR ui

--- src/main/kotlin/ui/SudokuUI.kt
+++ src/main/kotlin/ui/SudokuUI.kt
@@ -5,14 +5,22 @@ import javafx.scene.input.KeyCode
 import javafx.scene.layout.BorderPane
 import javafx.scene.layout.GridPane
 import tornadofx.View
+import tornadofx.addClass
 import tornadofx.onChange
 
 class SudokuUI : View() {
     override val root: BorderPane by fxml()
+    private val g1: GridPane by fxid()
+    private val g2: GridPane by fxid()
+    private val g3: GridPane by fxid()
+    private val g4: GridPane by fxid()
+    private val g5: GridPane by fxid()
+    private val g6: GridPane by fxid()
+    private val g7: GridPane by fxid()
+    private val g8: GridPane by fxid()
+    private val g9: GridPane by fxid()
 
-    private val grid: GridPane by fxid()
-
-    private val cells = arrayListOf<Cell>()
+    private val grids = listOf(g1, g2, g3, g4, g5, g6, g7, g8, g9)
 
     init {
         title = "Sudoku Solver"
@@ -21,37 +29,52 @@ class SudokuUI : View() {
             isResizable = false
         }
 
+        grids.forEach { it.addClass(Styles.grid) }
+
+    }
+
+
+    private val cells = arrayListOf<Cell>()
+
+
+    init {
         repeat(9) { i ->
             repeat(9) { j ->
                 val child = Cell(i, j)
 
                 with(child) {
                     textProperty().onChange {
-                        selectCell(row, col+1)
+                        if (!text.isEmpty())
+                            selectCell(row, col + 1)
                     }
 
                     onKeyPressed = EventHandler {
                         when (it.code) {
-                            KeyCode.ENTER, KeyCode.RIGHT -> selectCell(row, col+1)
-                            KeyCode.LEFT -> selectCell(row+8, col+8)
-                            KeyCode.UP -> selectCell(row+8, col)
-                            KeyCode.DOWN -> selectCell(row+1, col)
+                            KeyCode.BACK_SPACE -> {
+                                clear()
+                                selectCell(row + 8, col + 8)
+                            }
+                            KeyCode.DELETE -> clear()
+                            KeyCode.ENTER, KeyCode.RIGHT -> selectCell(row, col + 1)
+                            KeyCode.LEFT -> selectCell(row + 8, col + 8)
+                            KeyCode.UP -> selectCell(row + 8, col)
+                            KeyCode.DOWN -> selectCell(row + 1, col)
                             else -> it.consume()
                         }
                     }
                 }
 
-                grid.add(child, j, i)
+                val gridId = 3 * (i / 3) + j / 3
+                grids[gridId].add(child, j % 3, i % 3)
                 cells.add(child)
             }
         }
     }
 
-    private fun selectCell(row: Int, col: Int){
-        val id = (9*row + col) % cells.size
+    private fun selectCell(row: Int, col: Int) {
+        val id = (9 * row + col) % cells.size
         cells[id].requestFocus()
     }
-
 
     fun solve() {
         println("solve")
