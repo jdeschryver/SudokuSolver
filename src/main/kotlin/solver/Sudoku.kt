29081ceb23e7647e50c24d62b7584b19ded6548c
Jan De Schryver <Jan.DeSchryver@ugent.be>
2017-09-09 12:02:44 +0200

    sudoku representation

---
+++ src/main/kotlin/solver/Sudoku.kt
@@ -0,0 +1,24 @@
+package solver
+
+/**
+ * @author Jan De Schryver <Jan.DeSchryver@bucephalus.be>
+ */
+
+
+data class Cell(var value: Int = -1, val editable: Boolean = true, var possibilities: MutableList<Int> = mutableListOf(1,2,3,4,5,6,7,8,9))
+
+class Sudoku {
+    private val matrix: MutableList<Cell> = mutableListOf()
+    init {
+        for(x in 0..8){
+            for(y in 0..8){
+                matrix[matrixIndex(x,y)] = Cell()
+            }
+        }
+    }
+
+    fun getCell(c: Pair<Int, Int>): Cell? = matrix[matrixIndex(c.first, c.second)]
+
+    // Coordinates top left are (0,0), bottom right (8,8)
+    private fun matrixIndex(x: Int, y: Int) = x * 9 + y
+}
--- src/main/kotlin/solver/SudokuSolver.kt
+++ src/main/kotlin/solver/SudokuSolver.kt
@@ -1,5 +1,18 @@
+import solver.Sudoku
+
 /**
  * @author Jan De Schryver <Jan.DeSchryver@bucephalus.be>
  */
 
+class SudokuSolver{
 
+    val sudoku = Sudoku()
+
+    fun solve(){
+
+    }
+}
+
+fun main(args: Array<String>) {
+    println("Hello, world!")
+}
