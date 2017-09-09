f0ed9dbbd1a39f6ee967ddbdb398a83407ac819c
Jan De Schryver <Jan.DeSchryver@ugent.be>
2017-09-09 21:33:57 +0200

    untested solve

--- src/main/kotlin/solver/Sudoku.kt
+++ src/main/kotlin/solver/Sudoku.kt
@@ -8,16 +8,9 @@ package solver
 data class Cell(var value: Int = -1, val editable: Boolean = true, var possibilities: MutableList<Int> = mutableListOf(1,2,3,4,5,6,7,8,9))
 
 class Sudoku {
-    private val matrix: MutableList<Cell> = mutableListOf()
-    init {
-        repeat(9) { x ->
-            repeat(9) { y ->
-                matrix[matrixIndex(x,y)] = Cell()
-            }
-        }
-    }
+    private val matrix: Array<Cell> = Array(9 * 9) { Cell() }
 
-    fun getCell(c: Pair<Int, Int>): Cell? = matrix[matrixIndex(c.first, c.second)]
+    fun getCell(c: Pair<Int, Int>): Cell = matrix[matrixIndex(c.first, c.second)]
 
     // Coordinates top left are (0,0), bottom right (8,8)
     private fun matrixIndex(x: Int, y: Int) = x * 9 + y
--- src/main/kotlin/solver/SudokuSolver.kt
+++ src/main/kotlin/solver/SudokuSolver.kt
@@ -1,4 +1,7 @@
 import solver.Sudoku
+import java.util.ArrayList
+
+
 
 /**
  * @author Jan De Schryver <Jan.DeSchryver@bucephalus.be>
@@ -8,18 +11,28 @@ class SudokuSolver{
 
     val sudoku = Sudoku()
 
-    fun solve(){
-
+    fun solve(c: Pair<Int, Int>) : Boolean{
+        if(c.second < 8){
+            val cell = sudoku.getCell(c)
+            for (value in cell.possibilities) {
+                cell.value = value
+                if (solve(nextCell(c))) {
+                    return true
+                }
+            }
+            return false
+        }
+        return true
     }
 
-    fun deletePossibility(c: Pair<Int, Int>, value: Int){
+    fun deletePossibility(c: Pair<Int, Int>, value: Int) {
         val blockX = Math.floor(c.first / 3.0).toInt() * 3
         val blockY = Math.floor(c.second / 3.0).toInt() * 3
 
         repeat(9) { x ->
             val range = controlLoop(c, x, blockX, blockY)
             for(y in range.first..range.second){
-
+                sudoku.getCell(Pair(x, y)).possibilities.remove(value)
             }
         }
     }
@@ -28,6 +41,12 @@ class SudokuSolver{
         c.first -> Pair(0,8)
         in blockX..blockX+3 -> Pair(blockY, blockY+3)
         else -> Pair(c.second, c.second)
+    }
+
+    fun nextCell(c: Pair<Int, Int>): Pair<Int, Int>{
+        val x = (c.first + 1) % 9
+        val y = if (x == 0) c.second+1 else c.second
+        return Pair(x, y)
     }
 }
 
