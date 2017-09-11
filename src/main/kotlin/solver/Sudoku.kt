71aee4fe24c42fc69f3388fc33f23217423ff233
Jan De Schryver <Jan.DeSchryver@UGent.be>
2017-09-11 22:10:56 +0200

    a little cleanup

--- src/main/kotlin/solver/Sudoku.kt
+++ src/main/kotlin/solver/Sudoku.kt
@@ -5,17 +5,46 @@ import org.apache.commons.lang3.StringUtils
 /**
  * @author Jan De Schryver <Jan.DeSchryver@bucephalus.be>
  */
+sealed class Cell(var value: Int)
+data class staticCell(private val v: Int) : Cell(v)
+data class editableCell(private val v: Int = -1,
+                        val possibilities: MutableList<Int> = mutableListOf(1, 2, 3, 4, 5, 6, 7, 8, 9)) : Cell(v)
 
-
-data class Cell(var value: Int = -1, val editable: Boolean = true, val possibilities: MutableList<Int> = mutableListOf(1, 2, 3, 4, 5, 6, 7, 8, 9))
 
 class Sudoku {
-    private val matrix: Array<Cell> = Array(9 * 9) { Cell() }
+    private val matrix: Array<Cell> = Array(9 * 9) { editableCell() }
 
     fun getCell(c: Pair<Int, Int>): Cell = matrix[matrixIndex(c.first, c.second)]
 
     // Coordinates top left are (0,0), bottom right (8,8)
     private fun matrixIndex(x: Int, y: Int) = x * 9 + y
+
+    fun init(setup: List<Triple<Int, Int, Int>>){
+        for ((x,y, value) in setup){
+            matrix[matrixIndex(x,y)] = staticCell(value)
+        }
+    }
+
+    fun deletePossibility(c: Pair<Int, Int>, value: Int) {
+        val blockX = Math.floor(c.first / 3.0).toInt() * 3
+        val blockY = Math.floor(c.second / 3.0).toInt() * 3
+
+        repeat(9) { x ->
+            val range = controlLoop(c, x, blockX, blockY)
+            for (y in range.first..range.second) {
+                val cell = getCell(Pair(x, y))
+                if( cell is editableCell){
+                    cell.possibilities.remove(value)
+                }
+            }
+        }
+    }
+
+    private fun controlLoop(c: Pair<Int, Int>, x: Int, blockX: Int, blockY: Int): Pair<Int, Int> = when (x) {
+        c.first -> Pair(0, 8)
+        in blockX..blockX + 3 -> Pair(blockY, blockY + 3)
+        else -> Pair(c.second, c.second)
+    }
 
     fun toPrettyString(): String {
         val line = StringUtils.repeat('-', 5)
--- src/main/kotlin/solver/SudokuSolver.kt
+++ src/main/kotlin/solver/SudokuSolver.kt
@@ -1,4 +1,5 @@
 import solver.Sudoku
+import solver.editableCell
 
 /**
  * @author Jan De Schryver <Jan.DeSchryver@bucephalus.be>
@@ -11,35 +12,22 @@ class SudokuSolver {
     fun solve(c: Pair<Int, Int>): Boolean {
         if (c.second < 8) {
             val cell = sudoku.getCell(c)
-            for (value in cell.possibilities) {
-                cell.value = value
-                deletePossibility(c, value)
-                if (solve(nextCell(c))) {
-                    return true
+            if(cell is editableCell) {
+                for (value in cell.possibilities) {
+                    cell.value = value
+                    sudoku.deletePossibility(c, value)
+                    if (solve(nextCell(c))) {
+                        return true
+                    }
                 }
+            }else if (solve(nextCell(c))) {
+                return true
             }
             return false
         }
         return true
     }
 
-    private fun deletePossibility(c: Pair<Int, Int>, value: Int) {
-        val blockX = Math.floor(c.first / 3.0).toInt() * 3
-        val blockY = Math.floor(c.second / 3.0).toInt() * 3
-
-        repeat(9) { x ->
-            val range = controlLoop(c, x, blockX, blockY)
-            for (y in range.first..range.second) {
-                sudoku.getCell(Pair(x, y)).possibilities.remove(value)
-            }
-        }
-    }
-
-    private fun controlLoop(c: Pair<Int, Int>, x: Int, blockX: Int, blockY: Int): Pair<Int, Int> = when (x) {
-        c.first -> Pair(0, 8)
-        in blockX..blockX + 3 -> Pair(blockY, blockY + 3)
-        else -> Pair(c.second, c.second)
-    }
 
     private fun nextCell(c: Pair<Int, Int>): Pair<Int, Int> {
         var nextC: Pair<Int, Int>
@@ -47,13 +35,13 @@ class SudokuSolver {
             val x = (c.first + 1) % 9
             val y = if (x == 0) c.second + 1 else c.second
             nextC = Pair(x, y)
-        } while (sudoku.getCell(nextC).editable)
+        } while (sudoku.getCell(nextC) is editableCell)
 
         return nextC
     }
 }
 
 fun main(args: Array<String>) {
-    println("Hello, world!")
+    val sudoku = Sudoku()
     println(Sudoku().toPrettyString())
 }
